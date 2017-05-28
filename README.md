ec9d16e0f3296649de56b167b15b17c294e2cc71
Albert <albertauyeung@gmail.com>
2017-05-28 17:28:38 +0800

    added readme

---
+++ README.md
@@ -0,0 +1,47 @@
+# Implementation of Matrix Factorization in Python
+
+The source code [mf.py](mf.py) is an implementation of the matrix factorization algorithm in Python, using stochastic gradient descent. An article with detailed explanation of the algorithm can be found at [http://www.albertauyeung.com/post/python-matrix-factorization/](http://www.albertauyeung.com/post/python-matrix-factorization/).
+
+Below is an example of using the algorithm:
+
+```python
+import numpy as np
+from mf import MF
+
+# A rating matrix with ratings from 5 users on 4 items
+# zero entries are unknown values
+R = np.array([
+    [5, 3, 0, 1],
+    [4, 0, 0, 1],
+    [1, 1, 0, 5],
+    [1, 0, 0, 4],
+    [0, 1, 5, 4],
+])
+
+# Perform training and obtian the 
+mf = MF(R, K=2, alpha=0.1, beta=0.01, iterations=20)
+training_process = mf.train()
+print(mf.P)
+print(mf.Q)
+print(mf.full_matrix())
+
+# Prints the following:
+'''
+[[ 1.45345236  0.06946249]
+ [ 1.12922538  0.2319001 ]
+ [-1.21051208  0.94619099]
+ [-0.93607816  0.43182699]
+ [-0.6919936  -0.93611985]]
+
+[[ 1.42787151 -0.20548935]
+ [ 0.84792614  0.29530697]
+ [ 0.18071811 -1.2672859 ]
+ [-1.4211893   0.20465575]]
+ 
+[[ 4.98407556  2.99856476  3.96309763  1.01351377]
+ [ 3.99274702  2.27661831  3.20365416  1.0125506 ]
+ [ 1.0064803   1.00498576  2.37696737  4.98530109]
+ [ 1.00999456  0.59175173  2.58437035  3.99597255]
+ [ 2.26471556  1.01985428  4.9871617   3.9942251 ]]
+'''
+```
---
+++ mf.py
@@ -0,0 +1,94 @@
+import numpy as np
+
+
+class MF():
+    
+    def __init__(self, R, K, alpha, beta, iterations):
+        """
+        Perform matrix factorization to predict empty
+        entries in a matrix.
+        
+        Arguments
+        - R (ndarray)   : user-item rating matrix
+        - K (int)       : number of latent dimensions
+        - alpha (float) : learning rate
+        - beta (float)  : regularization parameter
+        """
+        
+        self.R = R
+        self.num_users, self.num_items = R.shape
+        self.K = K
+        self.alpha = alpha
+        self.beta = beta
+        self.iterations = iterations
+
+    def train(self):
+        # Initialize user and item latent feature matrice
+        self.P = np.random.normal(scale=1./self.K, size=(self.num_users, self.K))
+        self.Q = np.random.normal(scale=1./self.K, size=(self.num_items, self.K))
+        
+        # Initialize the biases
+        self.b_u = np.zeros(self.num_users)
+        self.b_i = np.zeros(self.num_items)
+        self.b = np.mean(self.R[np.where(self.R != 0)])
+        
+        # Create a list of training samples
+        self.samples = [
+            (i, j, self.R[i, j])
+            for i in range(self.num_users)
+            for j in range(self.num_items)
+            if self.R[i, j] > 0
+        ]
+        
+        # Perform stochastic gradient descent for number of iterations
+        training_process = []
+        for i in range(self.iterations):
+            np.random.shuffle(self.samples)
+            self.sgd()
+            mse = self.mse()
+            training_process.append((i, mse))
+            if (i+1) % 10 == 0:
+                print("Iteration: %d ; error = %.4f" % (i+1, mse))
+        
+        return training_process
+
+    def mse(self):
+        """
+        A function to compute the total mean square error
+        """
+        xs, ys = self.R.nonzero()
+        predicted = self.full_matrix()
+        error = 0
+        for x, y in zip(xs, ys):
+            error += pow(self.R[x, y] - predicted[x, y], 2)
+        return np.sqrt(error)
+
+    def sgd(self):
+        """
+        Perform stochastic graident descent
+        """
+        for i, j, r in self.samples:
+            # Computer prediction and error
+            prediction = self.get_rating(i, j)
+            e = (r - prediction)
+            
+            # Update biases
+            self.b_u[i] += self.alpha * (e - self.beta * self.b_u[i])
+            self.b_i[j] += self.alpha * (e - self.beta * self.b_i[j])
+            
+            # Update user and item latent feature matrices
+            self.P[i, :] += self.alpha * (e * self.Q[j, :] - self.beta * self.P[i,:])
+            self.Q[j, :] += self.alpha * (e * self.P[i, :] - self.beta * self.Q[j,:])
+
+    def get_rating(self, i, j):
+        """
+        Get the predicted rating of user i and item j
+        """
+        prediction = self.b + self.b_u[i] + self.b_i[j] + self.P[i, :].dot(self.Q[j, :].T)
+        return prediction
+    
+    def full_matrix(self):
+        """
+        Computer the full matrix using the resultant biases, P and Q
+        """
+        return mf.b + mf.b_u[:,np.newaxis] + mf.b_i[np.newaxis:,] + mf.P.dot(mf.Q.T)
